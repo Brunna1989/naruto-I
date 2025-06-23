c2ef21d23b89b92e2c239ac695ab24fef1645134
brunna.dornelles <brunnadornelles336@gmail.com>
2025-06-23 17:57:24 -0300

    Adicionando dependências do MapStruct, para devido uso de Mappers em Java 17 ou superior e plugins do Lombok MapStruct Biding para reforçar que o uso de mapeamentos entre Lombok e MapStruct sejam realizados de forma correta.

---
+++ src/main/java/com/desafio/narutoI/NarutoIApplication.java
@@ -0,0 +1,13 @@
+package com.desafio.narutoI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NarutoIApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NarutoIApplication.class, args);
+	}
+
+}
