b898576505ac18e47a301683c2c31bfa107ff7b0
brunna.dornelles <brunnadornelles336@gmail.com>
2025-06-23 18:24:21 -0300

    Adição de entidade Personagem.

---
+++ src/main/java/com/desafio/narutoI/entidades/Personagem.java
@@ -0,0 +1,33 @@
+package com.desafio.narutoI.entidades;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "tipo_ninja")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public abstract class Personagem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    private int idade;
+
+    private String aldeia;
+
+    private int chakra;
+
+    @ElementCollection
+    private List<String> jutsus = new ArrayList<>();
+}
