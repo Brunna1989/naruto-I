01d137d204678c6f15ac71a673e2fc7007e0d9f8
brunna.dornelles <brunnadornelles336@gmail.com>
2025-06-26 16:24:56 -0300

    Refatoração nas entidades.

--- src/main/java/com/desafio/narutoI/entidades/NinjaDeNinjutsu.java
+++ src/main/java/com/desafio/narutoI/entidades/NinjaDeNinjutsu.java
@@ -3,17 +3,40 @@ package com.desafio.narutoI.entidades;
 import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
 
+import java.util.Random;
+
 @Entity
 @DiscriminatorValue("NINJUTSU")
 public class NinjaDeNinjutsu extends Personagem implements Ninja {
 
     @Override
-    public void usarJutsu() {
-        System.out.println(getNome() + " está usando um jutsu de Ninjutsu!");
+    public void usarJutsu(String nomeJutsu, Personagem oponente) {
+        Jutsu jutsu = getJutsus().get(nomeJutsu);
+        if (jutsu == null) {
+            System.out.println(getNome() + " não conhece o jutsu: " + nomeJutsu);
+            return;
+        }
+        if (getChakra() < jutsu.getConsumoDeChakra()) {
+            System.out.println(getNome() + " não tem chakra suficiente para usar " + nomeJutsu);
+            return;
+        }
+
+        setChakra(getChakra() - jutsu.getConsumoDeChakra());
+        System.out.println(getNome() + " lançou um jutsu de Ninjutsu: " + nomeJutsu + ", causando " + jutsu.getDano() + " de dano!");
+        oponente.desviar(jutsu.getDano());
     }
 
     @Override
-    public void desviar() {
-        System.out.println(getNome() + " está desviando de um ataque usando Ninjutsu!");
+    public void desviar(int danoRecebido) {
+        boolean desviou = new Random().nextBoolean();
+        if (desviou) {
+            System.out.println(getNome() + " desviou com ninjutsu defensivo!");
+        } else {
+            setVida(getVida() - danoRecebido);
+            System.out.println(getNome() + " recebeu o ataque e perdeu " + danoRecebido + " de vida!");
+        }
     }
+
+
 }
+
--- src/main/java/com/desafio/narutoI/entidades/NinjaDeTaijutsu.java
+++ src/main/java/com/desafio/narutoI/entidades/NinjaDeTaijutsu.java
@@ -3,17 +3,38 @@ package com.desafio.narutoI.entidades;
 import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
 
+import java.util.Random;
+
 @Entity
 @DiscriminatorValue("TAIJUTSU")
 public class NinjaDeTaijutsu extends Personagem implements Ninja {
 
     @Override
-    public void usarJutsu() {
-        System.out.println(getNome() + " está usando um golpe de Taijutsu!");
+    public void usarJutsu(String nomeJutsu, Personagem oponente) {
+        Jutsu jutsu = getJutsus().get(nomeJutsu);
+        if (jutsu == null) {
+            System.out.println(getNome() + " não conhece o jutsu: " + nomeJutsu);
+            return;
+        }
+        if (getChakra() < jutsu.getConsumoDeChakra()) {
+            System.out.println(getNome() + " não tem chakra suficiente para usar " + nomeJutsu);
+            return;
+        }
+
+        setChakra(getChakra() - jutsu.getConsumoDeChakra());
+        System.out.println(getNome() + " atacou com " + nomeJutsu + ", causando " + jutsu.getDano() + " de dano!");
+        oponente.desviar(jutsu.getDano());
     }
 
     @Override
-    public void desviar() {
-        System.out.println(getNome() + " está desviando de um ataque usando Taijutsu!");
+    public void desviar(int danoRecebido) {
+        boolean desviou = new Random().nextBoolean();
+        if (desviou) {
+            System.out.println(getNome() + " desviou do ataque com técnica de Taijutsu!");
+        } else {
+            setVida(getVida() - danoRecebido);
+            System.out.println(getNome() + " foi atingido e perdeu " + danoRecebido + " de vida!");
+        }
     }
 }
+
