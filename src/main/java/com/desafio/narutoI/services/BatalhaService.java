98d326270f68755764265c4baf6e0971e766fa71
brunna.dornelles <brunnadornelles336@gmail.com>
2025-06-27 10:09:47 -0300

    Pensando na lógica - adição de classe Service.

---
+++ src/main/java/com/desafio/narutoI/services/BatalhaService.java
@@ -0,0 +1,28 @@
+package com.desafio.narutoI.services;
+
+import com.desafio.narutoI.entidades.Personagem;
+import com.desafio.narutoI.repositories.PersonagemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class BatalhaService {
+
+    @Autowired
+    private PersonagemRepository personagemRepository;
+
+    public String iniciarBatalha(Long idAtacante,String nomeJutsu,Long idDefensor) {
+        Optional<Personagem>atacanteOpt = personagemRepository.findByid(idAtacante);
+        Optional<Personagem>defensorOpt = personagemRepository.findById(idDefensor);
+
+        if (atacanteOpt.isEmpty() || defensorOpt.isEmpty()) {
+            return "Atacante ou defensor não encontrado.";
+        }
+    }
+
+
+
+
+}
