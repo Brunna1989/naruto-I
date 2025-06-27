a0e603f4eb0ea33fa9cf5ed078a9b9ddb3cd00e0
brunna.dornelles <brunnadornelles336@gmail.com>
2025-06-27 13:25:34 -0300

    Inserção da classe de exceção ChakraInsuficienteException.

---
+++ src/main/java/com/desafio/narutoI/exception/ChakraInsuficienteException.java
@@ -0,0 +1,7 @@
+package com.desafio.narutoI.exception;
+
+public class ChakraInsuficienteException extends RuntimeException{
+    public ChakraInsuficienteException(String mensagem) {
+        super(mensagem);
+    }
+}
