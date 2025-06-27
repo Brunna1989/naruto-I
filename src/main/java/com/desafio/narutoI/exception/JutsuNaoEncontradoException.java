3a4de378931706287128f8ed203378bf93fae8c2
brunna.dornelles <brunnadornelles336@gmail.com>
2025-06-27 13:37:05 -0300

    Adição da classe JutsuNaoEncontradoException.

---
+++ src/main/java/com/desafio/narutoI/exception/JutsuNaoEncontradoException.java
@@ -0,0 +1,8 @@
+package com.desafio.narutoI.exception;
+
+public class JutsuNaoEncontradoException extends RuntimeException {
+    public JutsuNaoEncontradoException(String mensagem) {
+        super(mensagem);
+    }
+}
+
